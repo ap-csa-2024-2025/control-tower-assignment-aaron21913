ed04ffd90973bc805b28ff3325fdd24e75e68376
aaron21913 <atngo106@student.hbuhsd.edu>
2024-10-11 10:50:05 -0700

    airplanerizz

--- Main.java
+++ Main.java
@@ -7,14 +7,24 @@ public class Main
    // Complete the code below
    
    // TODO: Create Airplane 1 and 2
-   
+   Airplane airplane1 = new Airplane();
+   Airplane airplane2 = new Airplane(AAA02, 15.8, 128, 0, 0);
    // TODO: Create a Scanner
    
    // TODO: Getting inputs for Airplane 3
    System.out.println("Enter the details of the third airplane (call-sign, distance, bearing and altitude): ");
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("Enter call sign for Airplane 3");
+   String callSign =  input.nextLine();
+   System.out.println("Enter distance for Airplane 3");
+   Double distance = input.nextDouble();
+   System.out.println("Enter direction for Airplane 3");
+   int direction = input.nextInt();
+   System.out.println("Enter the altitude for Airplane 3");
+   int altitude = input.nextInt();
 
    // TODO: Create Airplane 3 from inputs
-   
+   Airplane airplane3 = new Airplane(callSign, distance, direction, altitude);
    // TODO: Print the initial positions
    System.out.println("\nInitial Positions:");
    
